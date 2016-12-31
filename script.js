69b4d1cdcac8233f70e3b835b3e6d838b89168f8
Yoong Ti Lee <yoongtilee@yoongs-mbp.attlocal.net>
2016-12-31 01:42:49 -0600

    added animation for nav and button click

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-$(document).ready(function(){
-    $('.js-section-features').waypoint(function(direction){
-        if(direction == "down"){
+$(document).ready(function () {
+    $('.js-section-features').waypoint(function (direction) {
+        if (direction == "down") {
             $('nav').addClass('sticky');
-        }else{
+        } else {
             $('nav').removeClass('sticky');
         }
-    },{
+    }, {
         offset: '60px;'
     });
+
+    // SCROLL FOR BUTTONS
+    $('.js-scroll-to-plan').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.section-plan').offset().top
+        }, 1000);
+    });
+
+    $('.js-scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.section-features').offset().top
+        }, 1000);
+    });
+
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
 });
